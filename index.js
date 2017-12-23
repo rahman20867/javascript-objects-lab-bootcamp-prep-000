7b4623c6f09eed50ecb4e150d2b62ce1a4a6fbe7
Marvin Andrade <marvin@lionguardsecurity.com>
2017-12-23 01:21:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {recipes:"none"};
+
+function updateObjectWithKeyAndValue(object, key, value){
+    object.key = 'value';
+
+return object
+  }
+
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  obj[key] = value
+
+return obj
+}
